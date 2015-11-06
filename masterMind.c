206ee01e0c57016f9cd28fab571a3c03a0e90a3e
Sean Bartholomew <sbartholomew@localhost.localdomain>
2015-11-06 16:18:06 -0500

    Added comments

--- masterMind.c
+++ masterMind.c
@@ -18,6 +18,13 @@ void asChars(char*, int);
 #define TOTAL 10000
 #define OUTCOMES 14
 
+
+/*
+ *Program to play the Master Mind game. A random 4 digit number is chosen, and the user is asked to input a guess. Feedback is given in the form of # Red # White where red is the number of characters in the correct spot, and white is the number of characters in the incorrect spot. 
+ *As implemented the program also implements Knuth's algorithm to solve in the least number of guesses. The suggested guess for is printed to the b ottom of the screen. The user is free to choose their own guess. 
+ */
+
+
 int main(void){
 	srand(time(NULL));	
 	char secretNum[MAXSIZE];
@@ -47,6 +54,11 @@ int main(void){
 	free(remaining);	
 }
 
+/*
+ *getGuess returns an int that is the error status of the function. 0 = no error. 1 = too much or too little data input. 2 = non numeric input.
+ *Function gets input from the user and verify's that it is a series of 4 digits.
+ *
+ */
 
 int getGuess(char* userGuess){
 	printf("Guess a number:");
@@ -70,6 +82,12 @@ int getGuess(char* userGuess){
 	}	
 	return returnValue;
 	}
+/*
+ *checkNums returns an int corelating to the red / white value of theGuess compared to the Secret. 
+ *
+ *
+ */
+
 
 int checkNums(char* theSecret, char* theGuess){
 	int numRed = 0;
@@ -101,6 +119,11 @@ int checkNums(char* theSecret, char* theGuess){
 	return toIndex(numRed, numWhite);
 }
 
+/*
+ *dumpLine is used to clear the stdin buffer when/if the user inputs too much data.
+ *
+ */
+
 void dumpLine(FILE* filePointer){
 	printf("dumpline");
 	int temp = '0';
@@ -110,6 +133,15 @@ void dumpLine(FILE* filePointer){
   		*/
 	}
 }
+
+/*
+ *playSelf implements Knuth's algorithm to solve the Mastermind game. Function takes pointers to arrays for the remaining valid numbers and the last guess.
+ *The algorithm allocates an array large enough to hold an integer for every possible outcome of every possible guess.
+ *The algorithm compares every possible guess (numbers 0-9999) to every remaining possible value. In doing so an array is created which details the possible outcomes of every guess. The data for each outcome details how many numbers of those remaining will still be valid if that is the return value. 
+ *The algorithm uses that information to choose the next guess based on what number will guarantee to remove the MOST values from the current list of numbers.
+ * see also wikipedia -- Mastermind
+ * 
+ */
 
 void playSelf(char* guess, int* remaining){
 	int* allPossible[OUTCOMES];
@@ -156,9 +188,12 @@ void playSelf(char* guess, int* remaining){
 		free(allPossible[j]);
 	}
 	asChars(guess, nextGuess);
-	printf("%s\n", guess);
+	printf("Suggested Guess: %s\n", guess);
 }
-
+/*
+ *returns a unique value for every possible value of red and white. 
+ *inverse of printResult()
+ */
 
 int toIndex(int red, int white){
 	if (red == 1 || red == 2){
@@ -174,6 +209,12 @@ int toIndex(int red, int white){
 		return white;
 	}
 }
+
+/*
+ *takes a unique value corresponding to a red/ white number and prints the red / white value to the screen.
+ *inverse of toIndex()
+ *
+ */
 
 void printResult(int index){
 	if ( index < MAXSIZE ) {
@@ -192,14 +233,17 @@ void printResult(int index){
 		printf("4 red\n");
 	}
 }
-
+/*
+ *Array[] is an array of numbers where the index correspons to a guess value. The value stored at the index is either 0 or 1. 1 corresponds to a 
+ *number that is still a possibility in Knuth's Algorithm. the string guess and the int guessResult correspond to the last guess and it's unique index value as determined by toIndex()
+ *For every number that is currently still valid, the function checks to see if it is a possible value for the secret number if not it is marked as a 0
+ */
  
 void stillValid(int* array, char* guess, int guessResult){
 	int i;
 	for (i =0; i < TOTAL; i++){
 		char value[(MAXSIZE-1)];
 		asChars(value, i);
-	//	sprintf(value, "%d", i);
 		bool inPlay = (array[i] == 1);
 		if(value == guess){
 			inPlay = false;
@@ -214,7 +258,10 @@ void stillValid(int* array, char* guess, int guessResult){
 		}
 	}
 }
-
+/*
+ *
+ *Function takes a string pointer and a number, and changes the string to correspond to a 4 digit value of the number e.x. 7 is "0007" 
+ */
 
 void asChars(char* asString, int number){
 	if(number < 10){
